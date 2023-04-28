2b3b593468c02b3dcbd47f01c82a444fb2a4577d
George <george.cairney9831@gmail.com>
2023-04-27 20:17:56 -0700

    Added a new set of files, following the official tutorial from next.js

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,9 +1,13 @@
 import Image from 'next/image'
 import Head from 'next/head'
+import Link from 'next/link';
+
 import { prefix } from 'prefix.js';
 import { Inter } from 'next/font/google'
 
 const inter = Inter({ subsets: ['latin'] })
+
+
 
 export default function Home() {
   return (
@@ -19,56 +23,13 @@ export default function Home() {
             Get started by editing&nbsp;
             <code className="font-mono font-bold">src/pages/index.js</code>
           </p>
-          <div className="fixed bottom-0 left-0 flex items-end justify-center w-full h-48 bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-            <a
-              className="flex gap-2 p-8 pointer-events-none place-items-center lg:pointer-events-auto lg:p-0"
-              href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              By{' '}
-              <Image
-                src={`${prefix}/vercel.svg`}
-                alt="Vercel Logo"
-                className="dark:invert"
-                width={100}
-                height={24}
-                priority
-              />
-            </a>
-          </div>
         </div>
 
-        <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700/10 after:dark:from-sky-900 after:dark:via-[#0141ff]/40 before:lg:h-[360px]">
-          <Image
-            className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-            src={`${prefix}/next.svg`}
-            alt="Next.js Logo"
-            width={180}
-            height={37}
-            priority
-          />
-        </div>
 
         <div className="grid mb-32 text-center lg:mb-0 lg:grid-cols-4 lg:text-left">
-          <a
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-            className="px-5 py-4 transition-colors border border-transparent rounded-lg group hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <h2 className={`${inter.className} mb-3 text-2xl font-semibold`}>
-              Docs{' '}
-              <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-                -&gt;
-              </span>
-            </h2>
-            <p
-              className={`${inter.className} m-0 max-w-[30ch] text-sm opacity-50`}
-            >
-              Find in-depth information about Next.js features and API.
-            </p>
-          </a>
+            <h1 className="title">
+                Read <Link href="/posts/first-post">this page!</Link>
+            </h1>
 
           <a
             href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
