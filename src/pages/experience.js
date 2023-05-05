a556009dd8fab79c17b3fb319c9fc135598417f0
George <george.cairney9831@gmail.com>
2023-05-04 20:45:28 -0700

    outlining the main pages to know what I'm planning

--- src/pages/experience.js
+++ src/pages/experience.js
@@ -1,8 +1,21 @@
 import React, {Component} from "react";
+import Layout from "./components/layout";
 
 export default function Experience(){
     return (
-        <>
-        </>
+        <Layout>
+            <div>
+                Professional Experience
+            </div>
+            <div>
+                Tech Stack
+            </div>
+            <div>
+                Software I have worked with
+            </div>
+            <div>
+                Future career interests
+            </div>
+        </Layout>
     );
 }
--- src/pages/projects.js
+++ src/pages/projects.js
@@ -1,8 +1,18 @@
 import React, {Component} from "react";
+import Layout from "./components/layout";
 
 export default function Projects(){
     return (
-        <>
-        </>
+        <Layout>
+            <div>
+                This one
+            </div>
+            <div>
+                Destiny 2 Bot
+            </div>
+            <div>
+                Future Projects?
+            </div>
+        </Layout>
     );
 }
